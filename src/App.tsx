df79abc5c49835e48f8da2f77083920737e0df0d
Julian Londono <jlond10@gmail.com>
2020-09-25 10:00:57 -0400

    Begin communications component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,24 @@ import React from 'react';
 import './App.css';
 import Navbar from './Navbar'
 import ActionBar from './ActionBar'
+import Communication from './Communication'
 
 export default function App() {
   return (
-    <div className="container-fluid pl-0 pr-0">
-      <Navbar />
-      <ActionBar />
+    <div className="container-fluid px-0 h-100">
+      <div className="row">
+        <div className="col-md-12">
+          <Navbar />
+        </div>
+      </div>
+      <div className="row d-flex h-100">
+        <div className="col-md-9">
+          <ActionBar />
+        </div>
+        <div className="col-md-3">
+          <Communication />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Communication.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+export default function ActionBar() {
+    return (
+        <div className="container-fluid px-0  bg-light h-100">
+            <div className="row">
+                <div className="col-md-12">
+                    <p>Comments</p>
+                </div>
+            </div>
+        </div>
+    );
+}
